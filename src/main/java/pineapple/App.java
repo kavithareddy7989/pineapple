dc6074a9ee48aee2062be3565279b6d7cf9a5e65
root <root@ip-172-31-1-177.us-east-2.compute.internal>
2022-01-01 07:38:45 +0000

    target

---
+++ src/main/java/pineapple/App.java
@@ -0,0 +1,13 @@
+package pineapple;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
